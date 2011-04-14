dc9d10a0f2041037b5e5b8dbe9bb12fe3866ba30
Yury Brodskiy <Y.Brodskiy@utwente.nl>
2011-04-14 21:30:26 +0200

    Next iteration of intraction cotnrol

--- youbot_common/youbot_description/src/20_sim_interaction_control/common/xxtypes.h
+++ youbot_common/youbot_description/src/20_sim_interaction_control/common/xxtypes.h
@@ -5,7 +5,7 @@
  *  model: ControlYoubotFullInteraction_Grav_v1
  *  expmt: ControlYoubotFullInteraction_Grav_v1
  *  date:  April 14, 2011
- *  time:  8:04:34 pm
+ *  time:  9:28:09 pm
  *  user:  Campuslicentie 
  *  from:  Universiteit Twente
  *  build: 4.1.2.2
--- youbot_common/youbot_description/src/20_sim_interaction_control/test_youbot_interaction_control.cpp
+++ youbot_common/youbot_description/src/20_sim_interaction_control/test_youbot_interaction_control.cpp
@@ -6,7 +6,7 @@
  *  model: ControlYoubotFullInteraction_Grav_v1
  *  expmt: ControlYoubotFullInteraction_Grav_v1
  *  date:  April 14, 2011
- *  time:  8:04:34 pm
+ *  time:  9:28:09 pm
  *  user:  Campuslicentie
  *  from:  Universiteit Twente
  *  build: 4.1.2.2
@@ -49,26 +49,22 @@ int main()
 	u[13] = 0.0;
 	u[14] = 0.0;
 	u[15] = 0.0;
-
-	u[16] = 1;		/* setPointH */
+	u[16] = 0.0;		/* setPointH */
 	u[17] = 0.0;
 	u[18] = 0.0;
 	u[19] = 0.0;
-
 	u[20] = 0.0;
-	u[21] = 1.0;
+	u[21] = 0.0;
 	u[22] = 0.0;
 	u[23] = 0.0;
-
 	u[24] = 0.0;
 	u[25] = 0.0;
-	u[26] = 1.0;
+	u[26] = 0.0;
 	u[27] = 0.0;
-
 	u[28] = 0.0;
 	u[29] = 0.0;
 	u[30] = 0.0;
-	u[31] = 1.0;
+	u[31] = 0.0;
 
 	y[0] = 0.0;		/* outputForces.e */
 	y[1] = 0.0;
@@ -78,22 +74,18 @@ int main()
 	y[5] = 0.0;
 	y[6] = 0.0;
 	y[7] = 0.0;
-
 	y[8] = 0.0;		/* tipH */
 	y[9] = 0.0;
 	y[10] = 0.0;
 	y[11] = 0.0;
-
 	y[12] = 0.0;
 	y[13] = 0.0;
 	y[14] = 0.0;
 	y[15] = 0.0;
-
 	y[16] = 0.0;
 	y[17] = 0.0;
 	y[18] = 0.0;
 	y[19] = 0.0;
-
 	y[20] = 0.0;
 	y[21] = 0.0;
 	y[22] = 0.0;
@@ -111,7 +103,6 @@ int main()
 		/* call the submodel to calculate the output */
 		my20simSubmodel.Calculate (u, y);
 		printf("Time: %f\n", my20simSubmodel.GetTime() );
-		printf("tipH test :%f, %f,%f,%f \t position: %f,%f,%f\n",y[8],y[13],y[18],y[23],y[11],y[15],y[19]);
 	}
 
 	/* perform the final calculations */
