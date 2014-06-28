d52e63a37d630f41aca16a1f156329dd40ab8f4c
Brad Hayes <hayesbh@gmail.com>
2014-06-27 21:19:12 -0400

    Changed tabs to spaces, made sure everything in youbot_oodl is 4-space indented

--- youbot_drivers/youbot_oodl/src/youbot_oodl.cpp
+++ youbot_drivers/youbot_oodl/src/youbot_oodl.cpp
@@ -42,67 +42,67 @@
 int main(int argc, char **argv)
 {
 
-  youbot::Logger::toConsole = false;
-  youbot::Logger::toFile = false;
-  youbot::Logger::toROS = true;
-  ros::init(argc, argv, "youbot_oodl_driver");
-  ros::NodeHandle n;
-  youBot::YouBotOODLWrapper youBot(n);
-  std::vector<std::string> armNames;
+    youbot::Logger::toConsole = false;
+    youbot::Logger::toFile = false;
+    youbot::Logger::toROS = true;
+    ros::init(argc, argv, "youbot_oodl_driver");
+    ros::NodeHandle n;
+    youBot::YouBotOODLWrapper youBot(n);
+    std::vector<std::string> armNames;
 
 
-  /* configuration */
-  bool youBotHasBase;
-  bool youBotHasArms;
-  double youBotDriverCycleFrequencyInHz;  //the driver recives commands and publishes them with a fixed frequency
-  n.param("youBotHasBase", youBotHasBase, true);
-  n.param("youBotHasArms", youBotHasArms, true);
-  n.param("youBotDriverCycleFrequencyInHz", youBotDriverCycleFrequencyInHz, 50.0);
-  n.param<std::string>("youBotConfigurationFilePath", youBot.youBotConfiguration.configurationFilePath, mkstr(YOUBOT_CONFIGURATIONS_DIR));
-  n.param<std::string>("youBotBaseName", youBot.youBotConfiguration.baseConfiguration.baseID, "youbot-base");
+    /* configuration */
+    bool youBotHasBase;
+    bool youBotHasArms;
+    double youBotDriverCycleFrequencyInHz;  //the driver recives commands and publishes them with a fixed frequency
+    n.param("youBotHasBase", youBotHasBase, true);
+    n.param("youBotHasArms", youBotHasArms, true);
+    n.param("youBotDriverCycleFrequencyInHz", youBotDriverCycleFrequencyInHz, 50.0);
+    n.param<std::string>("youBotConfigurationFilePath", youBot.youBotConfiguration.configurationFilePath, mkstr(YOUBOT_CONFIGURATIONS_DIR));
+    n.param<std::string>("youBotBaseName", youBot.youBotConfiguration.baseConfiguration.baseID, "youbot-base");
 
 
-  // Retrieve all defined arm names from the launch file params
-  int i = 1;
-  std::stringstream armNameParam;
-  armNameParam << "youBotArmName" << i; // youBotArmName1 is first checked param... then youBotArmName2, etc.
-  while (n.hasParam(armNameParam.str())) {
-    std::string armName;
-    n.getParam(armNameParam.str(), armName);
-    armNames.push_back(armName);
-    armNameParam.str("");
-    armNameParam << "youBotArmName" <<  (++i);
-  }
-
-  ros::ServiceServer reconnectService = n.advertiseService("reconnect", &youBot::YouBotOODLWrapper::reconnectCallback, &youBot);
-
-  ROS_ASSERT((youBotHasBase == true) || (youBotHasArms == true)); // At least one should be true, otherwise nothing to be started.
-  if (youBotHasBase == true)
-  {
-    youBot.initializeBase(youBot.youBotConfiguration.baseConfiguration.baseID);
-  }
-
-  if (youBotHasArms == true) {
-    std::vector<std::string>::iterator armNameIter;
-    for (armNameIter = armNames.begin(); armNameIter != armNames.end(); ++armNameIter) {
-      youBot.initializeArm(*armNameIter);
+    // Retrieve all defined arm names from the launch file params
+    int i = 1;
+    std::stringstream armNameParam;
+    armNameParam << "youBotArmName" << i; // youBotArmName1 is first checked param... then youBotArmName2, etc.
+    while (n.hasParam(armNameParam.str())) {
+        std::string armName;
+        n.getParam(armNameParam.str(), armName);
+        armNames.push_back(armName);
+        armNameParam.str("");
+        armNameParam << "youBotArmName" <<  (++i);
     }
-  }
- 
 
-  /* coordination */
-  ros::Rate rate(youBotDriverCycleFrequencyInHz); //Input and output at the same time... (in Hz)
-  while (n.ok())
-  {
-    ros::spinOnce();
-    youBot.computeOODLSensorReadings();
-    youBot.publishOODLSensorReadings();
-    youBot.publishArmAndBaseDiagnostics(2.0);    //publish only every 2 seconds
-    rate.sleep();
-  }
+    ros::ServiceServer reconnectService = n.advertiseService("reconnect", &youBot::YouBotOODLWrapper::reconnectCallback, &youBot);
 
-  youBot.stop();
+    ROS_ASSERT((youBotHasBase == true) || (youBotHasArms == true)); // At least one should be true, otherwise nothing to be started.
+    if (youBotHasBase == true)
+    {
+        youBot.initializeBase(youBot.youBotConfiguration.baseConfiguration.baseID);
+    }
 
-  return 0;
+    if (youBotHasArms == true) {
+        std::vector<std::string>::iterator armNameIter;
+        for (armNameIter = armNames.begin(); armNameIter != armNames.end(); ++armNameIter) {
+            youBot.initializeArm(*armNameIter);
+        }
+    }
+
+
+    /* coordination */
+    ros::Rate rate(youBotDriverCycleFrequencyInHz); //Input and output at the same time... (in Hz)
+    while (n.ok())
+    {
+        ros::spinOnce();
+        youBot.computeOODLSensorReadings();
+        youBot.publishOODLSensorReadings();
+        youBot.publishArmAndBaseDiagnostics(2.0);    //publish only every 2 seconds
+        rate.sleep();
+    }
+
+    youBot.stop();
+
+    return 0;
 }
 
